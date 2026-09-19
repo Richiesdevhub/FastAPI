1f2b57ce9f0815e4cb234c072d6a52fab7f9cd80
Richiegithub <rislfagmail.com>
2026-09-18 18:09:42 -0600

    new tectWithTim rework project

---
+++ TechWithTim001/app/app.py
@@ -0,0 +1,25 @@
+from fastapi import FastAPI, HTTPException
+
+app = FastAPI()
+
+text_posts={"1": {"title": "Post 1", "content": "Content of post 1"},
+            "2": {"title": "Post 2", "content": "Content of post 2"},
+            "3": {"title": "Post 3", "content": "Content of post 3"}
+            }
+
+@app.get("/hello_world")
+def hello_world():
+    return {"message": "Hello World"}
+
+@app.get("/posts")
+def get_all_posts(limit:int=None):
+    if limit:
+        return list(text_posts.values())[:limit]
+    return list(text_posts.values())
+
+@app.get("/posts/{id}")
+def get_post(id:str):
+    if id not in text_posts:
+        return HTTPException(status_code=404, detail="Post not found")
+       
+    return text_posts.get(id)
