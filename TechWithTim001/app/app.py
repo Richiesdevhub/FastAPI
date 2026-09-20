0398937878ee9288c17b67218889d1ccccdfa404
Richiegithub <rislfagmail.com>
2026-09-19 22:18:52 -0600

    Erase code for test without database

--- TechWithTim001/app/app.py
+++ TechWithTim001/app/app.py
@@ -11,30 +11,3 @@ async def lifespan(app: FastAPI):
 
 app = FastAPI(lifespan=lifespan)
 
-text_posts={1: {"title": "Post 1", "content": "Content of post 1"},
-            2: {"title": "Post 2", "content": "Content of post 2"},
-            3: {"title": "Post 3", "content": "Content of post 3"}
-            }
-
-@app.get("/hello_world")
-def hello_world():
-    return {"message": "Hello World"}
-
-@app.get("/posts")
-def get_all_posts(limit:int=None):
-    if limit:
-        return list(text_posts.values())[:limit]
-    return list(text_posts.values())
-
-@app.get("/posts/{id}")
-def get_post(id:str)->list[PostResponse]:
-    if id not in text_posts:
-        return HTTPException(status_code=404, detail="Post not found")
-       
-    return text_posts.get(id)
-
-@app.post("/posts")
-def create_post(post:PostCreate)->PostResponse:
-    new_post={"title":post.title, "content":post.content}
-    text_posts[max(text_posts.keys())+1] = new_post
-    return new_post
