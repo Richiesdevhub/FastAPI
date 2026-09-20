643186dfe6d0c9c46198a8df37795ed56e928721
Richiegithub <rislfagmail.com>
2026-09-19 22:15:46 -0600

    Terminamos db.py y con las relaciones creamos la base de datos test

--- TechWithTim001/app/db.py
+++ TechWithTim001/app/db.py
@@ -1,6 +1,6 @@
-from collections import ASyncGenerator
+from collections.abc import AsyncGenerator
 import uuid
-from sqlalchemy import Column, String, Text, DateTime, Foreignkey
+from sqlalchemy import Column, String, Text, DateTime, ForeignKey
 from sqlalchemy.dialects.postgresql import UUID
 from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine, async_sessionmaker
 from sqlalchemy.orm import DeclarativeBase, relationship
@@ -31,9 +31,9 @@ async_session_maker=async_sessionmaker(engine, expire_on_commit=False)
 #funcion que crea la base de datos y tablas
 async def create_db_and_tables():
     async with engine.begin() as conn:
-        await conn.run_sync(DeclarativeBase.metadata.create_all)
+        await conn.run_sync(Base.metadata.create_all)
 
 #Funcion que crea una sesion asincrona para leer y escribir en la base de datos
-async def get_async_session() -> ASyncGenerator[AsyncSession, None]:
+async def get_async_session() -> AsyncGenerator[AsyncSession, None]:
     async with async_session_maker() as session:
         yield session
